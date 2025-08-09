04b7906f0182afbf11a20ba9d515a977864a8a6f
OctetularZ <taiabayomi15@gmail.com>
2025-08-09 20:34:39 +0100

    Completed basic card layout. Will start working on hover overlay

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -2,7 +2,14 @@ import React from "react"
 
 const ProjectCard = () => {
   return (
-    <div className="w-80 h-80 bg-transparent border-1 border-coffee-300 m-10"></div>
+    <a href="#">
+      <div className="w-80 h-80 bg-transparent border-1 border-coffee-300 m-10 flex items-center justify-center">
+        <img
+          src="/projects/placeholder.svg"
+          className="h-10/12 w-10/12 border-1 border-coffee-300"
+        />
+      </div>
+    </a>
   )
 }
 
